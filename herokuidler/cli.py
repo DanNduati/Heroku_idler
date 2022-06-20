d0b290a23cbb04d75fbdbde6c864e8534b56c7f7
DanielNduati <dannduati2@gmail.com>
2022-06-20 21:35:08 +0300

    Implement init cli command
    The command takes an optional database file path and creates the
    application configuration file and the json storage

--- herokuidler/cli.py
+++ herokuidler/cli.py
@@ -1,10 +1,39 @@
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from herokuidler import __app_name__, __version__
+from herokuidler import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        "--db-path",
+        "-db",
+        prompt="use this as the url json storage location?",
+    )
+) -> None:
+    """Initialize the url json storage"""
+    config_init_error = config.init_app(db_path)
+    if config_init_error:
+        typer.secho(
+            f'Creating config file failed with "{ERRORS[config_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"The urls json storage is {db_path}", fg=typer.colors.GREEN)
 
 
 def _version_callback(value: bool) -> None:
