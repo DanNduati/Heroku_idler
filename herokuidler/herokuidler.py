9e60810cd938b98d10fe5282a6632df5d1fbb2df
DanielNduati <dannduati2@gmail.com>
2022-06-21 14:43:04 +0300

    Implement the add() function and refactor url structure
    - The UrlController.add() function takes a url as a parameter and
    appends it to the url list
    - Using .write_urls of the storage handler writes the updated list back
    to the json storage
    - Returns an instance of the CurrentUrl with the url and the appropriate
    return code

--- herokuidler/herokuidler.py
+++ herokuidler/herokuidler.py
@@ -1,14 +1,25 @@
 from pathlib import Path
-from typing import Any, Dict, NamedTuple
+from typing import Any, Dict, List, NamedTuple
 
+from herokuidler import DB_READ_ERROR
 from herokuidler.database import StorageHandler
 
 
 class CurrentUrl(NamedTuple):
-    todo: Dict[str, Any]
+    url: List[str]
     error: int
 
 
 class UrlController:
     def __init__(self, db_path: Path) -> None:
         self._storage_handler = StorageHandler(db_path)
+
+    def add(self, url: str) -> CurrentUrl:
+        """Add a new url to the database"""
+        url = {"url": url}
+        read = self._storage_handler.read_urls()
+        if read.error == DB_READ_ERROR:
+            return CurrentUrl(url, read.error)
+        read.url_list.append(url)
+        write = self._storage_handler.write_urls(read.url_list)
+        return CurrentUrl(url, write.error)
