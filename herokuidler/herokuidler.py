95fd3fb6f8654b98f1d38665e32eec3a4624fc64
DanielNduati <dannduati2@gmail.com>
2022-06-21 12:44:27 +0300

    Setup mock_json_file() fixture
    This fixture creates and returns a temporary JSON file with a single url
    in it. In the fixture we also use the
    [tmp_path](https://docs.pytest.org/en/6.2.x/tmpdir.html#the-tmp-path-fixture) fixture which provides a temporary directory unique to the test invocation.

--- herokuidler/herokuidler.py
+++ herokuidler/herokuidler.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from typing import Any, Dict, NamedTuple
+
 from herokuidler.database import StorageHandler
+
 
 class CurrentUrl(NamedTuple):
     todo: Dict[str, Any]
     error: int
-    
+
+
 class UrlController:
-    def __init__(self,db_path:Path)-> None:
+    def __init__(self, db_path: Path) -> None:
         self._storage_handler = StorageHandler(db_path)
--- tests/test_herokuidler.py
+++ tests/test_herokuidler.py
@@ -1,6 +1,16 @@
+import json
+
+import pytest
 from typer.testing import CliRunner
 
-from herokuidler import __app_name__, __version__, cli
+from herokuidler import (  # isort:skip
+    DB_READ_ERROR,
+    SUCCESS,
+    __app_name__,
+    __version__,
+    cli,
+    herokuidler,
+)
 
 runner = CliRunner()
 
@@ -9,3 +19,12 @@ def test_version():
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}" in result.stdout
+
+
+@pytest.fixture
+def mock_json_file(tmp_path):
+    url = [{"url": "https://gentle-dusk-50795.herokuapp.com/ping"}]
+    db_file = tmp_path / "url.json"
+    with db_file.open("w") as db:
+        json.dump(url, db, indent=4)
+    return db_file
